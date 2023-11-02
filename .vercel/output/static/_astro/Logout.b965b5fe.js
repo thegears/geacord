15f777f302a90dc402c470a9b7ed17f511e1b6a9
thegears <semh.1362@gmail.com>
2023-11-02 04:22:41 +0300

    update

--- .vercel/output/static/_astro/Logout.b965b5fe.js
+++
@@ -1,1 +0,0 @@
-import{a as t,j as o}from"./user.a01d3f51.js";import{r}from"./index.6460afdd.js";function a(){return r.useEffect(()=>{t(),window.location.href="/"},[]),o.jsx(o.Fragment,{})}export{a as default};
