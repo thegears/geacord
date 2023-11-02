804b7723c1488e2ba9468b46ee2c8cfe82764fc0
thegears <semh.1362@gmail.com>
2023-11-02 04:21:57 +0300

    update

---
+++ .vercel/output/static/_astro/Logout.b965b5fe.js
@@ -0,0 +1,1 @@
+import{a as t,j as o}from"./user.a01d3f51.js";import{r}from"./index.6460afdd.js";function a(){return r.useEffect(()=>{t(),window.location.href="/"},[]),o.jsx(o.Fragment,{})}export{a as default};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import purgecss from "astro-purgecss";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
 
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), purgecss(), robotsTxt(), sitemap()],
-  output: "hybrid",
-  adapter: vercel()
+  integrations: [react(), tailwind(), robotsTxt(), sitemap()],
+
 });
